e00e6ad4699c13ff1da78e3ece6e52904a94e50c
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2023-11-28 19:03:44 -0300

    feat: add mounted

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 new Vue({
     el: '#app',
     vuetify: new Vuetify(),
+    methods: {
+        traerPeliculas: function () {
+            console.log('megameg');
+        }
+    },
+    mounted() {
+        this.traerPeliculas()
+    }
   })
